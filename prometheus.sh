c125202e9cf3d9825c0f40145a37332bdcffbc6a
Roberto Arcomano <bertolinux@gmail.com>
2022-09-24 23:02:17 +0200

    Added .gitignore as well as prometheus.sh

---
+++ prometheus.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# C. Constants
+NS=monitoring
+NOW="--grace-period=0 --force"
+
+# 0. Re-create namespace
+kubectl delete ns $NS $NOW
+kubectl create ns $NS
+
+# 1. Load cluster role as well as cluster role binding
+kubectl replace -f clusterRole.yaml --force
+
+# 2. Load config map
+kubectl replace -f config-map.yaml --force
+
+# 3. Load deployment
+kubectl replace -f prometheus-deployment.yaml --force
+
+# 4. Load server
+kubectl replace -f prometheus-service.yaml --force
+
+# 5. Load ingress
+kubectl replace -f prometheus-ingress.yaml --force
+
+# 6. Show everything
+kubectl get all -n $NS
